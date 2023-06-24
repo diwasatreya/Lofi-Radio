e2a2771a3f96f21c75525714e108db76418de491
Diwas Atreya <diwasatreya45@gmail.com>
2023-06-24 15:52:42 +0545

    updates

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 
 module.exports = {
-  token: process.env.TOKEN || '', // your discord bot token
-  prefix: process.env.PREFIX || '.', // bot prefix
-  ownerID: process.env.OWNERID?.split(',') || ['519666024220721152','965503342249914408'], //your discord id
-  SpotifyID: process.env.SPOTIFYID || '38eb449803a24a5fbb4fecdeff237b90', // spotify client id
-  SpotifySecret: process.env.SPOTIFYSECRET || '08dab901c92f4ad296a7156b68f5e048', // spotify client secret
-  mongourl: process.env.MONGO_URI || 'mongodb+srv://diwasatreya:radio@radio.hwvpd28.mongodb.net/', // MongoDb URL
-  embedColor: process.env.COlOR || '#DDBD86', // embed colour
-  logs: process.env.LOGS || '', // Discord channel id 
+  token: process.env.TOKEN || '', 
+  prefix: process.env.PREFIX || '.', 
+  ownerID: process.env.OWNERID?.split(',') || ['519666024220721152','965503342249914408'], 
+  SpotifyID: process.env.SPOTIFYID || '', 
+  SpotifySecret: process.env.SPOTIFYSECRET || '', 
+  mongourl: process.env.MONGO_URI || '', 
+  embedColor: process.env.COlOR || '#DDBD86', // 
+  logs: process.env.LOGS || '', 
   links: {
     support: process.env.SUPPORT || 'https://discord.gg/aromax-development-708565122188312579',
     invite: process.env.INVITE || 'https://discord.gg/aromax-development-708565122188312579',
